6d96871d23a0cfd90b8c69564b2840d457a4e0ec
AshirAli <ashirmayankalathil@gmail.com>
2019-01-26 17:29:25 +0530

    Start camera roll

--- Assets/Scripts/Timeline_Controller.cs
+++ Assets/Scripts/Timeline_Controller.cs
@@ -6,20 +6,44 @@ using UnityEngine.Playables;
 public class Timeline_Controller : MonoBehaviour
 {
     public PlayableDirector cameraControl;
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        PlayAnimCamera();
+        
     }
 
+    private void OnEnable()
+    {
+        cameraControl.stopped += OnPlayableDirectorStopped;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            PlayAnimCamera();
+        }
+    }
+
+    void OnPlayableDirectorStopped(PlayableDirector aDirector)
+    {
+        if (cameraControl == aDirector)
+        {
+            Debug.Log("PlayableDirector named " + aDirector.name + " is now stopped.");
+            player.SetActive(true);
+        }
+            
+            
     }
 
     public void PlayAnimCamera()
     {
         cameraControl.Play();
+    }
+    void OnDisable()
+    {
+        cameraControl.stopped -= OnPlayableDirectorStopped;
     }
 }
