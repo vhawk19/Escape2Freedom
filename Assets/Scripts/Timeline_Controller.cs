d8da445826602a8d33d2978b5a35a30206b5f3cc
AshirAli <ashirmayankalathil@gmail.com>
2019-01-26 17:01:39 +0530

    cinemachine_timeline

---
+++ Assets/Scripts/Timeline_Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Timeline_Controller : MonoBehaviour
+{
+    public PlayableDirector cameraControl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayAnimCamera();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayAnimCamera()
+    {
+        cameraControl.Play();
+    }
+}
